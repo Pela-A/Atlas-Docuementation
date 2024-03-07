0956726b871b6691ffbea72b76db492662ec354c
Pela-A <apela@email.neit.edu>
2024-03-07 17:13:40 -0500

    Site Created

---
+++ documentation.php
@@ -0,0 +1,112 @@
+<?php
+/*
+Introduction of each of the team members and a detailed description of the responsibilities of each member
+Your proposal in PDF or HTML
+prototype in PDF or HTML
+Technical Design Document in PDF or HTML
+Relationship Diagram of your normalized database in PDF or HTML
+Screenshots of your application (need to wait a little)
+Copy of the PowerPoint presentation (need to complete)
+Zipped file of all your source code (Wait until code done)
+
+create documentation downloads/pdf viewer
+
+
+acquired documentation))
+technical design document
+Database commands
+erd
+
+i need....
+proposal
+prototype
+
+
+carousel with screenshots of application and brief description
+
+downloadable zip file of all source code
+
+
+
+*/
+
+?>
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Atlas Documentation</title>
+  </head>
+  <body class="d-flex flex-column h-100">
+
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/atlasPhotos/ATLAS_Logo.png" style="width:50px;" alt="Logo">
+            
+                <strong>ATLAS</strong>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="documentation.php">Documentation</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    </header>
+    <main class="flex-shrink-0">
+        <div class="container" style="padding-top: 80px;">
+
+            <h1 class="text-center">The Atlas Project Documentation</h1>
+
+            <p>Here lies all of our documentation and file downloads.</p>
+
+            <h2>Zipped Code</h2>
+
+            <h2>Proposal</h2>
+
+            <h2>Prototype</h2>
+
+            <h2>User Interface Milestone Document</h2>
+            
+            <h2>MySQL Injects</h2>
+            <a href="pdfs/MySQL_Injects.pdf" download>Download PDF</a>
+            <embed src="pdfs/MySQL_Injects.pdf" type="application/pdf" width="100%" height="600px" />
+
+            <h2>Technical Design Document</h2>
+            <a href="pdfs/LMS_Technical_Design_.pdf" download>Download PDF</a>
+            <embed src="pdfs/LMS_Technical_Design_.pdf" type="application/pdf" width="100%" height="600px" />
+
+    </main>
+
+
+
+
+    <footer class="footer mt-auto py-3 bg-dark">
+        <div class="container">
+            <span class="text-muted row col-12">Alexander Pela and Tristan Knott.</span>
+            <a href="https://github.com/Pela-A/Atlas-Docuementation" class="row">Documentation Repo Link</a>
+            <a href="https://github.com/Pela-A/Learning-Management-System" class="row">Atlas Project Link</a>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
